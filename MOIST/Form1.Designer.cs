1dbb497a2195395079db82e846bda4be69694221
ModernAesthetics <32524851+ModernAesthetics@users.noreply.github.com>
2018-06-12 15:10:36 -0700

    Add files via upload

---
+++ MOIST/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MOIST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nsTheme1 = new NSTheme();
+            this.nsControlButton1 = new NSControlButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSCRAPE = new NSButton();
+            this.txtURL = new NSTextBox();
+            this.nsLabel1 = new NSLabel();
+            this.nsTheme1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nsTheme1
+            // 
+            this.nsTheme1.AccentOffset = 0;
+            this.nsTheme1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.nsTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.nsTheme1.Colors = new Bloom[0];
+            this.nsTheme1.Controls.Add(this.nsLabel1);
+            this.nsTheme1.Controls.Add(this.nsControlButton1);
+            this.nsTheme1.Controls.Add(this.groupBox2);
+            this.nsTheme1.Controls.Add(this.groupBox1);
+            this.nsTheme1.Customization = "";
+            this.nsTheme1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nsTheme1.Font = new System.Drawing.Font("Verdana", 8F);
+            this.nsTheme1.Image = null;
+            this.nsTheme1.Location = new System.Drawing.Point(0, 0);
+            this.nsTheme1.Movable = true;
+            this.nsTheme1.Name = "nsTheme1";
+            this.nsTheme1.NoRounding = false;
+            this.nsTheme1.Sizable = true;
+            this.nsTheme1.Size = new System.Drawing.Size(401, 404);
+            this.nsTheme1.SmartBounds = true;
+            this.nsTheme1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.nsTheme1.TabIndex = 0;
+            this.nsTheme1.Text = "EMAIL SCRAPER";
+            this.nsTheme1.TransparencyKey = System.Drawing.Color.Empty;
+            this.nsTheme1.Transparent = false;
+            // 
+            // nsControlButton1
+            // 
+            this.nsControlButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nsControlButton1.ControlButton = NSControlButton.Button.Close;
+            this.nsControlButton1.Location = new System.Drawing.Point(375, 3);
+            this.nsControlButton1.Margin = new System.Windows.Forms.Padding(0);
+            this.nsControlButton1.MaximumSize = new System.Drawing.Size(18, 20);
+            this.nsControlButton1.MinimumSize = new System.Drawing.Size(18, 20);
+            this.nsControlButton1.Name = "nsControlButton1";
+            this.nsControlButton1.Size = new System.Drawing.Size(18, 20);
+            this.nsControlButton1.TabIndex = 2;
+            this.nsControlButton1.Text = "nsControlButton1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtEmail);
+            this.groupBox2.ForeColor = System.Drawing.Color.SpringGreen;
+            this.groupBox2.Location = new System.Drawing.Point(12, 104);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(377, 265);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "EMAILS";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 22);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEmail.Size = new System.Drawing.Size(344, 229);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSCRAPE);
+            this.groupBox1.Controls.Add(this.txtURL);
+            this.groupBox1.ForeColor = System.Drawing.Color.SpringGreen;
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 55);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "URL";
+            // 
+            // btnSCRAPE
+            // 
+            this.btnSCRAPE.Location = new System.Drawing.Point(285, 19);
+            this.btnSCRAPE.Name = "btnSCRAPE";
+            this.btnSCRAPE.Size = new System.Drawing.Size(75, 23);
+            this.btnSCRAPE.TabIndex = 1;
+            this.btnSCRAPE.Text = "SCRAPE";
+            this.btnSCRAPE.Click += new System.EventHandler(this.btnSCRAPE_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.AutoScroll = false;
+            this.txtURL.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtURL.Location = new System.Drawing.Point(16, 19);
+            this.txtURL.MaxLength = 32767;
+            this.txtURL.Multiline = false;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.NumberBounds = new System.Drawing.Point(0, 0);
+            this.txtURL.NumbersOnly = false;
+            this.txtURL.ReadOnly = false;
+            this.txtURL.Size = new System.Drawing.Size(263, 23);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtURL.UseSystemPasswordChar = false;
+            // 
+            // nsLabel1
+            // 
+            this.nsLabel1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.nsLabel1.Location = new System.Drawing.Point(121, 374);
+            this.nsLabel1.Name = "nsLabel1";
+            this.nsLabel1.Size = new System.Drawing.Size(161, 23);
+            this.nsLabel1.TabIndex = 3;
+            this.nsLabel1.Text = "nsLabel1";
+            this.nsLabel1.Value1 = "MODERN";
+            this.nsLabel1.Value2 = "AESTHETICS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 404);
+            this.Controls.Add(this.nsTheme1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.nsTheme1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NSTheme nsTheme1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private NSTextBox txtURL;
+        private NSButton btnSCRAPE;
+        private NSControlButton nsControlButton1;
+        private NSLabel nsLabel1;
+    }
+}
+
---
+++ MOIST/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace MOIST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSCRAPE_Click(object sender, EventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string data = client.DownloadString(txtURL.Text);
+                string pattern = @"[A-Za-z0-9_\-\+]+@[A-Za-z0-9_\-]+\.([A-Za-z]{2,3})(?:\.[a-z]{2})?";
+
+                MatchCollection matches = Regex.Matches(data, pattern);
+
+                if(matches.Count > 0)
+                {
+                    foreach(Match m in matches)
+                    {
+                        txtEmail.AppendText(m.Groups[0].Value + Environment.NewLine);
+                    }
+                }
+            }
+
+        }
+    }
+}
